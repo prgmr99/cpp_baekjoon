93300065e4ef40daee70209320b2be2a3fe41610
SeungJun Yeom <johnyeom24@naver.com>
2021-11-10 12:19:02 +0900

    1번 풀이: 2차원 배열을 사용하지 않고 풀기 2번 풀이: 2차원 배열을 사용하여 풀기 백준에서 1번 풀이는 틀렸다고 하지만 vs에서는 컴파일 오류없고 값도 잘나옴.

--- github2/bj11098.cpp
+++ github2/bj11098.cpp
@@ -4,5 +4,53 @@ using namespace std;
 
 int main()
 {
+	int tcase = 0;
+	cin >> tcase;
+
+	int pnum = 0;
+	int cost;
+	char pname[20];
+	int max = 0;
+	char ename[20];
+
+	for (int i = 0; i < tcase; i++)
+	{
+		cin >> pnum;
+		for (int j = 0; j < pnum; j++)
+		{
+			cin >> cost >> pname;
+			if (cost > max) {
+				max = cost;
+				*ename = *pname;
+			}
+		}
+		cout << *pname << endl;
+	}
 	return 0;
 }
+
+/*int main()
+{
+	int i, j, n, p;
+	char a[100][21] = {};
+	cin >> n;
+
+	for (i = 0; i < n; i++)
+	{
+		int max = 0, cost, l;
+		cin >> p;
+		for (j = 0; j < p; j++)
+		{
+			cin >> cost;
+			cin >> a[j];
+
+			if (cost > max)
+			{
+				max = cost;
+				l = j;
+			}
+		}
+		cout << a[l] << endl;
+	}
+	return 0;
+}*/
