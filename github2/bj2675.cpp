d8a58111ae221aa3dd5dc48d4e2565762a929d3b
SeungJun Yeom <johnyeom24@naver.com>
2021-11-21 17:17:54 +0900

    이지요

---
+++ github2/bj2675.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(void)
+{
+	int R,T;
+	string S;
+	//int sLen=S.length(); 여기에 위치하면 안된다.
+	cin >> T;
+	for (int k = 0; k < T; k++)
+	{
+		cin >> R >> S;
+		int sLen=S.length();    // 변수 선언 위치 아무리 c++이라도 막무가네로 위치시키면 안된다.
+		for (int i = 0; i < sLen; i++)
+			for (int j = 0; j < R; j++)
+				cout << S[i];
+		cout << endl;
+	}
+	return 0;
+}
+/*int main(void) {
+	//ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, R;
+	string S;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> R >> S;
+		for (int j = 0; j < S.length(); j++)
+			for (int k = 0; k < R; k++)
+				cout << S[j];
+		cout << '\n';
+	}
+}*/
