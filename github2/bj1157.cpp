444eb7b055a9f98d14f981f919d36b08ff849f34
SeungJun Yeom <johnyeom24@naver.com>
2021-11-24 23:43:16 +0900

    오랜만에 먹방

--- github2/bj1157.cpp
+++ github2/bj1157.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
 // 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 
 // 단, 대문자와 소문자를 구분하지 않는다.
 
+
+// 나의 풀이
 // 배열을 만들고 배열의 인덱스를 (int)str[i] 이런식으로 하면 cnt++; 가능.
-int arr[122];
+/*int arr[122];
 
 int main(void)
 {
@@ -37,5 +40,52 @@ int main(void)
 		cout << '?' << endl;
 	else
 		cout << max << endl;
+	return 0;
+}*/
+// 애초에 문제를 잘못 읽고 풀었다.
+// 가장 많이 적힌 알파벳의 대문자를 출력해야한다.
+
+int main(void)
+{
+	string str;
+	int arr[26] = { 0, };
+	int max_cnt = 0;
+	int index = 0;
+	int count = 0;
+
+	cin >> str;
+	for (int i = 0; i < str.length(); i++)
+	{
+		int n = str.at(i);
+
+		if (n < 97)
+			arr[n - 65]++;  
+		else
+			arr[n - 97]++;  
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (arr[i] > max_cnt)
+		{
+			max_cnt = arr[i];
+			index = i;
+		}
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (arr[i] == max_cnt)
+		{
+			count++;
+			if (count >= 2)
+			{
+				cout << "?" << endl;
+				return 0;
+			}
+		}
+	}
+	cout << (char)(index + 65) << endl;
+
 	return 0;
 }
