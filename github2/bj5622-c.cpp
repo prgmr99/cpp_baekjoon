61de7c9e3ddf8ff3cbad88f0bbb31cd406064c1a
SeungJun Yeom <johnyeom24@naver.com>
2021-11-26 11:21:47 +0900

    hi

---
+++ github2/bj5622-c.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// ³» Ç®ÀÌ.
+// ´Ü¼øÇÑµ¥ ±ò²û.
+/*int main(void)
+{
+	string str;
+	cin >> str;
+
+	int i, cnt = 0;
+
+	for (i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'C')
+			cnt += 3;
+		if (str[i] >= 'D' && str[i] <= 'F')
+			cnt += 4;
+		if (str[i] >= 'G' && str[i] <= 'I')
+			cnt += 5;
+		if (str[i] >= 'J' && str[i] <= 'L')
+			cnt += 6;
+		if (str[i] >= 'M' && str[i] <= 'O')
+			cnt += 7;
+		if (str[i] >= 'P' && str[i] <= 'S')
+			cnt += 8;
+		if (str[i] >= 'T' && str[i] <= 'V')
+			cnt += 9;
+		if (str[i] >= 'W' && str[i] <= 'Z')
+			cnt += 10;
+	}
+	cout << cnt << endl;
+	return 0;
+}*/
