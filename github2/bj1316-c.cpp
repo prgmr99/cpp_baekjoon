44bf66f382066b1f4590c5e9bc7a4fb89a0d0c18
SeungJun Yeom <johnyeom24@naver.com>
2021-11-28 14:49:04 +0900

    조금 어렵지만 문자열에서 bool자료형을 잘 이용하면 좋을 수 있다.

---
+++ github2/bj1316-c.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 그룹단어: 각 문자가 연속해서 나타나는 경우
+
+bool wordCheck(string str)
+{
+	bool alphabet[26] = { false };
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (alphabet[str[i] - 'a']) // true이면 이미 있었기 때문에 false. 그리고 a의 인덱스=0.
+			return false;
+		else                        // false이면 처음 오는 알파벳이므로 check
+		{
+			char tmp = str[i];      // 알파벳이 왔으면 true로 바꿔준다.
+			alphabet[str[i] - 'a'] = true;
+
+			while (1)
+			{
+				if (tmp != str[++i])// 현재 알파벳과 다른 알파벳이 올 때까지 탐색한다.
+				{
+					i--;
+					break;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+int main(void)
+{
+	int num;
+	cin >> num;
+	
+	int cnt = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		string str;
+		cin >> str;
+
+		if (wordCheck(str))
+			cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
