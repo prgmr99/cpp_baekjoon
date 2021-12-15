bdf7e92bb9407e403a8f74569ed4a7a9bc559d52
prgmr99 <johnyeom24@naver.com>
2021-12-15 23:09:53 +0900

    2869 더 빠르게 수행하는 코드 생각해보기

--- github2/bj2869.cpp
+++ github2/bj2869.cpp
@@ -7,24 +7,13 @@ int main(void)
 	int up, down, height, temp;
 	cin >> up >> down >> height;
 
+	temp = up;
 	for (int i = 1;; i++)
 	{
-		temp = up;
 		up *= i;
-		down *= -i;
-		/*if (i == 1) {
-			num1 = height - up;
-			num2 = up + down;
-		}*/
-
-		/*if (num1 % num2 == 0) {
-			day = num1 / num2;
-		}
-		else {
-			day = num1 / num2 + 1;
-		}*/
-
-		if ((up + down + temp) >= height)
+		down *= i;
+		
+		if ((up - down + temp) >= height)
 		{
 			cout << i + 1 << endl;
 			break;
