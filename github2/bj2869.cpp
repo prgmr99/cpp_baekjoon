465f505f3410527b8d154c24fd588b1d3d0ba5d1
prgmr99 <johnyeom24@naver.com>
2021-12-16 23:25:15 +0900

    반복문은 생각보다 오랜 시간이 걸린다.

--- github2/bj2869.cpp
+++ github2/bj2869.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main(void)
+/*int main(void)
 {
 	int up, down, height, temp;
 	cin >> up >> down >> height;
@@ -19,5 +19,25 @@ int main(void)
 			break;
 		}
 	}
+	return 0;
+}*/
+
+// 반복문을 사용하지 않고 더 빠르게 구현할 수 있는 코드를 생각해보자.
+// 반복문을 사용하면 무조건 시간초과하는 문제이다.
+
+// 내가 처음에 생각했던 방식(아래 코드)이 더 빠른 코드이다. 
+int main(void)
+{
+	int up, down, height;
+	cin >> up >> down >> height;
+
+	int day = 1;
+	day += (height - up) / (up - down);
+	if ((height - up) % (up - down) != 0)
+		day++;
+	if (up >= height)
+		cout << '1';
+	else
+		cout << day;
 	return 0;
 }
