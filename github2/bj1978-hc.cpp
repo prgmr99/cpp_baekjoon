677cfa184129d626f561821e24167ed38a87d5e7
prgmr99 <johnyeom24@naver.com>
2021-12-20 23:06:00 +0900

    소수 판별 알고리즘

--- github2/bj1978-hc.cpp
+++ github2/bj1978-hc.cpp
@@ -23,7 +23,7 @@ using namespace std;
 	return 0;
 }*/
 
-int main(void) {
+/*int main(void) {
 	int num;
 	cin >> num;
 	int cnt = num;
@@ -45,4 +45,31 @@ int main(void) {
 		}
 	cout << cnt;
 	return 0;
+}*/
+// 에라토스테네스의 접근 방법: 효율적이고 빠른 계산 가능
+// 에라토스테네스의 소수 필요충분조건은 2보다 크면서 자기 자신의
+// 제곱근까지의 수에 나눠지지 않는 수.
+// 즉, 입력값의 제곱근까지 반복문을 돌려 소수 판별
+
+// 아래의 코드와 같이도 활용할 수 있다.
+// 백준 문제의 해설은 아니지만 이런 식으로 풀 수 있다는 것도 참고.
+/*bool isPrime(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			cout << "소수 아님";
+			return false;
+		}
+	}
+	cout << "소수";
+	return true;
 }
+
+int main(void) {
+	for (int i = 2; i <= 100; i++) {
+		cout << i << ' ';
+		isPrime(i);
+		cout << endl;
+	}
+	return 0;
+}*/
