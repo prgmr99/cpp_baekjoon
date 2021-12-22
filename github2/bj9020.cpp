8fe2f4e8c9644907e222b05d5263e48360536b6e
prgmr99 <johnyeom24@naver.com>
2021-12-22 12:28:52 +0900

    조금 더 고민해보자

---
+++ github2/bj9020.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void) {
+	int tcase, num;
+	cin >> tcase;
+
+	for (int i = 0; i < tcase; i++) {
+		cin >> num;
+		for (int j = 2; j <= num; j++) {
+
+		}
+	}
+}
