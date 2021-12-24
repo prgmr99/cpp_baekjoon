d71c3a718c00937eed4ecfde5e00904b18e01416
prgmr99 <johnyeom24@naver.com>
2021-12-24 11:40:26 +0900

    확인!

--- github2/bj3009-c.cpp
+++ github2/bj3009-c.cpp
@@ -11,7 +11,7 @@ int main(void) {
 	cin >> x2 >> y2;
 	cin >> x3 >> y3;
 
-	if (x1 == x2)
+	/*if (x1 == x2)
 		x4 = x3;
 	if (x2 == x3)
 		x4 = x1;
@@ -23,8 +23,20 @@ int main(void) {
 	if (y2 == y3)
 		y4 = y1;
 	if (y1 == y3)
+		y4 = y2;*/
+	x4 = x1;
+	y4 = y1;
+	if (x4 == x2)
+		x4 = x3;
+	else if (x4 == x3)
+		x4 = x2;
+
+	if (y4 == y2)
+		y4 = y3;
+	else if (y4 == y3)
 		y4 = y2;
 
 	cout << x4 <<' '<< y4 << endl;
 	return 0;
 }
+
