6ef49903917776c1b35a8b03d622cbf28a49c303
prgmr99 <johnyeom24@naver.com>
2021-12-28 19:28:38 +0900

    하노이탑..부순다

--- github2/bj11729.cpp
+++ github2/bj11729.cpp
@@ -23,3 +23,16 @@ int main() {
 	cout << (int)pow(2, n) - 1 << '\n';
 	hanoi(1, 2, 3, n);
 }
+
+
+/*ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);*/
+
+// 코딩테스트를 하며 입출력의 속도 문제로 인해 Fail이 발생할 때,
+// 원리는 몰라도 위와 같은 코드를 써서 해결가능.
+
+// cin과 cout은 default로 tie되어 있다.
+// tie한다는 것은 두 개의 stream이 sync되어 있기 때문에 한 스트림에서 다른 스트림이 작업 요청을 하게 되면,
+// 작업했던 내용을 flush한다는 의미이다.
+// flush란 buffer에 쌓아둔 데이터를 모두 내보내는 것을 말한다.
