ae41939e0a40db0fdb3cc8bda1f522e588c84822
prgmr99 <johnyeom24@naver.com>
2021-12-29 20:13:36 +0900

    hh

--- github2/bj11650.cpp
+++ github2/bj11650.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int arr_x[100001];
+/*int arr_x[100001];
 int arr_y[100001];
 
 bool compare(int a, int b) {
@@ -19,5 +20,33 @@ int main(void) {
 	}
 	sort(arr_x, arr_x + tcase, compare);
 	sort(arr_y, arr_y + tcase, compare);
+	return 0;
+}*/
+// 더 좋은 도구가 있다.
+
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if (a.first == b.first)
+		return a.second < b.second;
+	return a.first < b.first;
+}
+
+int main(void) {
+	vector<pair<int, int>> vec;
+
+	int n;
+	int x, y;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		cin >> y;
+
+		vec.push_back(make_pair(x, y));
+	}
+
+	sort(vec.begin(), vec.end(), compare);
+	for (int i = 0; i < n; i++) {
+		cout << vec[i].first << ' ' << vec[i].second << '\n';
+	}
 	return 0;
 }
