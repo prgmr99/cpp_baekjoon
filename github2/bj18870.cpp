4da42b1936f9c29cab114fb1d396b38d06a363b4
prgmr99 <johnyeom24@naver.com>
2022-01-03 11:49:56 +0900

    why?

--- github2/bj18870.cpp
+++ github2/bj18870.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
+
+int main(void) {
+	int num, x, cnt = 0;
+	cin >> num;
+	vector<int> v(num);
+	int temp;
+	for (int i = 0; i < num; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
+	for (int i = 0; i < num; i++) {
+		temp = v[i];
+		for (int j = 0; j < num; j++) {
+			if (temp > v[j])
+				cnt++;
+		}
+		cout << cnt << ' ';
+	}
+	return 0;
+}
