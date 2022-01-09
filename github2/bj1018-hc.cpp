71796b3c344b83596961c2962c2928f52e28cac5
prgmr99 <johnyeom24@naver.com>
2022-01-09 17:46:58 +0900

    !

--- github2/bj1018-hc.cpp
+++ github2/bj1018-hc.cpp
@@ -23,6 +23,11 @@ string BW[8] = {
         "BWBWBWBW",
         "WBWBWBWB"
 };
+// 재색칠해야하는 횟수는 위의 두 가지의 체스판과 다른 색깔의 총합을 구하면 알 수 있다.
+// 간과하면 안되는 것. WB, BW를 모두 고려해야한다는 것.
+// 주어진 필드가 검은색으로 시작했다면 BW로 생긴 체스판과 비교를 하며 최솟값을 찾으려 한다.
+// 그런데 사실 제일 처음 필드를 W로 재색칠하고 시작하는 경우가 더 최솟값일 수 있다.
+
 string board[50];
 int WB_cnt(int x, int y)
 {
@@ -34,7 +39,6 @@ int WB_cnt(int x, int y)
             if (board[x + i][y + j] != WB[i][j])
                 cnt++;
         }
-
     }
     return cnt;
 }
@@ -48,7 +52,6 @@ int BW_cnt(int x, int y)
             if (board[x + i][y + j] != BW[i][j])
                 cnt++;
         }
-
     }
     return cnt;
 }
@@ -74,3 +77,7 @@ int main() {
     cout << min_val;
     return 0;
 }
+
+// brute force 방식으로 해결하면 되는 문제.
+// 모든 경우의 수를 모두 계산해본 후, 가장 재색칠이 적었던 경우를 출력해주면 된다.
+// 체스판의 형태는 2가지: 검정색으로 시작하는 경우, 흰색으로 시작하는 경우.
