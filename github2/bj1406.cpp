827bafa62e7d6b81d8d344b6cbb6c652039519b6
prgmr99 <johnyeom24@naver.com>
2022-01-18 15:02:12 +0900

    에디터 -> 커서의 위치 표현 고민

--- github2/bj1406.cpp
+++ github2/bj1406.cpp
@@ -3,16 +3,33 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#define pass (void) 0
 
 using namespace std;
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	string str;
 	cin >> str;
-	int n;
+	int n, cs;
 	cin >> n;
+	cs = n;
 	for (int i = 0; i < n; i++) {
-
+		string order;
+		cin >> order;
+		if (order == "L") {
+			cs -= 1;
+			if (cs == 0)
+				pass;
+		}
+		if (order == "D") {
+			cs += 1;
+			if(cs==)
+		}
 	}
 	return 0;
 }
+
+// cs의 위치를 표현방식을 조금 다르게 해야할 것 같아.
