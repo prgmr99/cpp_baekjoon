191f777f6997d8dc9c4b32ae1ca97a135407e5ca
prgmr99 <johnyeom24@naver.com>
2022-02-15 21:07:12 +0900

    long long을 통한 피보나치 수열 해결/dp 테이블 활용

--- github2/bj2748.cpp
+++ github2/bj2748.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
+long long fiboarr[100] = { 0,1, };
 
-int fibo(int num) {
-
+long long fibo(int num) {
+	if (num == 0 || num == 1)
+		return fiboarr[num];
+	else if (fiboarr[num] == 0)
+		fiboarr[num] = fibo(num - 1) + fibo(num - 2);
+	return fiboarr[num];
 }
 
 int main(void) {
-
+	int n;
+	cin >> n;
+	cout << fibo(n) << '\n';
+	return 0;
 }
