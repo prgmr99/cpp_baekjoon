30d4d8ced9049f0774492e7138a06ae1a6a8f284
prgmr99 <johnyeom24@naver.com>
2022-02-18 15:42:12 +0900

    fixed, precision(1) - 내 학점을 구해줘

--- github2/bj10984.cpp
+++ github2/bj10984.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+/*int main(void) {
 	int T, N, C = 0;
 	cin >> T;
-	cin >> N;
-	float G = 0.0;
+	
+	double G = 0.0;
 	while (T--) {
+		cin >> N;
 		int sumC = 0;
-		float sumG = 0.0;
+		double sumG = 0.0;
 		for (int i = 0; i < N; i++) {
 			cin >> C >> G;
 			sumC += C; sumG += G;
 		}
-		cout << sumC <<' '<< sumG/4 << '\n';
+		cout << sumC <<' '<< sumG/ (double)N << '\n';
+	}
+	return 0;
+}*/
+
+int main(void) {
+	int T;
+	cin >> T;
+
+	cout << fixed;
+	cout.precision(1);
+	for (int i = 0; i < T; i++) {
+		int N, score = 0, total = 0;
+		double gpa = 0.0, sgpa = 0.0;
+		cin >> N;
+		for (int j = 0; j < N; j++) {
+			cin >> score >> gpa;
+			total += score;
+			sgpa += (score*gpa);
+		}
+		cout << total << ' ' << sgpa / total << '\n';
 	}
 	return 0;
 }
